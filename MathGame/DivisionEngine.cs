80058d2b8b400cb81e8def8f3cfea29b87dca1f4
Cherno Alieu Cham <cchernoalieu@gmail.com>
2023-02-01 00:08:47 +0000

    Fixed the non-integer dividends

--- MathGame/DivisionEngine.cs
+++ MathGame/DivisionEngine.cs
@@ -16,18 +16,21 @@ namespace MathGame
 
 			int firstNumber;
 			int secondNumber;
+			
 
 			for (int j = 0; j < Helpers.GetNumberOfQuestions(); j++)
 			{
 				firstNumber = random.Next(1, 9);
 				secondNumber = random.Next(1, 9);
+				var product = firstNumber * secondNumber;
 
-				Console.WriteLine($"{firstNumber} / {secondNumber}");
+				Console.WriteLine($"{product} / {secondNumber}");
 				var result = Console.ReadLine();
 
 				result = Helpers.ValidationResult(result);
 
-				if (int.Parse(result) == firstNumber / secondNumber)
+
+				if (int.Parse(result) == product / secondNumber)
 				{
 					Console.WriteLine("Your answer is correct! Type any key for next question");
 					score++;
@@ -65,13 +68,14 @@ namespace MathGame
 			{
 				firstNumber = random.Next(10, 99);
 				secondNumber = random.Next(10, 99);
+				var product = firstNumber * secondNumber;
 
-				Console.WriteLine($"{firstNumber} / {secondNumber}");
+				Console.WriteLine($"{product} / {secondNumber}");
 				var result = Console.ReadLine();
 
 				result = Helpers.ValidationResult(result);
 
-				if (int.Parse(result) == firstNumber / secondNumber)
+				if (int.Parse(result) == product / secondNumber)
 				{
 					Console.WriteLine("Your answer is correct! Type any key for next question");
 					score++;
@@ -109,13 +113,14 @@ namespace MathGame
 			{
 				firstNumber = random.Next(100, 999);
 				secondNumber = random.Next(100, 999);
+				var product = firstNumber * secondNumber;
 
-				Console.WriteLine($"{firstNumber} / {secondNumber}");
+				Console.WriteLine($"{product} / {secondNumber}");
 				var result = Console.ReadLine();
 
 				result = Helpers.ValidationResult(result);
 
-				if (int.Parse(result) == firstNumber / secondNumber)
+				if (int.Parse(result) == product / secondNumber)
 				{
 					Console.WriteLine("Your answer is correct! Type any key for next question");
 					score++;
